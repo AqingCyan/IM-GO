daef986a25346e037789c7304b77680ae84ef98f
AqingCyan <aqingcyan@gmail.com>
2023-09-11 03:41:59 +0800

    feat: 完成互发消息

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 使用 Golang 基于 TCP 实现一个多人 IM System，架构如图所示：
 
 ![design](https://s2.loli.net/2023/09/11/cL8eAdMtYNCGXag.png)
+
+## 一阶段
+
+完成基本的登录与通信
+
+![demo1](https://s2.loli.net/2023/09/11/iwzWsgJjKO6bZVo.png)
--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -61,6 +62,30 @@ func (s *Server) Handler(conn net.Conn) {
 
 	// 广播当前用户上线消息
 	s.BroadCast(user, "已上线")
+
+	// 接收客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				// 当前用户下线
+				s.BroadCast(user, "下线")
+				return
+			}
+			if err != nil && err != io.EOF {
+				// 读取出错且不是 EOF
+				fmt.Println("conn read err:", err)
+				return
+			}
+
+			// 提取用户的消息（去除 '\n'）
+			msg := string(buf[:n-1])
+
+			// 将得到的消息进行广播
+			s.BroadCast(user, msg)
+		}
+	}()
 
 	// 当前 handler 阻塞
 	select {}
